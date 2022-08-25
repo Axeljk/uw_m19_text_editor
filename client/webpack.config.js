6c948c66467419a9c0445b91d5a3eb6cf0233a31
axeljk <axeljkern@yahoo.com>
2022-08-24 21:55:53 -0400

    Adds plugin for HTML and rules for css and babel.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,28 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+		new HtmlWebpackPlugin({
+			template: "./index.html",
+			title: "Webpack Plugin"
+		})
     ],
 
     module: {
       rules: [
-        
+		{
+			test: /\.css$/i,
+			use: ["style-loader", "css-loader"]
+		},
+		{
+			test: /\.m?js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: ["@babel/preset-env"]
+				}
+			}
+		}
       ],
     },
   };
