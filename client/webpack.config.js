3c70323050f37c079e4e9e9d2a0b59c60579f325
axeljk <axeljkern@yahoo.com>
2022-08-25 05:59:17 -0400

    Changes filename for service worker to be consistent.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = () => {
 		}),
 		new InjectManifest({
 			swSrc: "./src-sw.js",
-			swDest: "service-worker.js"
+			swDest: "src-sw.js"
 		}),
 		new WebpackPwaManifest({
 			name: "JATE",
